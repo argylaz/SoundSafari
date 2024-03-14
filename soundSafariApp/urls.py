5db0054d6ea62e033781e42f9b18ac18597ba968
Ijaz Saleem <2773056s@student.gla.ac.uk>
2024-03-14 20:30:09 +0000

    Base Implemented

--- soundSafariApp/urls.py
+++ soundSafariApp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from soundSafariApp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'SoundSafari'
 
 urlpatterns = [
     path('', views.index, name='index'),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
